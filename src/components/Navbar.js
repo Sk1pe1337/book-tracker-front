ded9c9e74dc7e75822a24aacd4f695fb232692ef
Sk1pe1337 <adilet.kudashev@bk.ru>
2025-02-24 17:13:08 +0500

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"; // Подключаем CSS
+import "./NavBar.css"; // Подключаем CSS
 
 function Navbar({ isAuthenticated, handleLogout }) {
   return (
